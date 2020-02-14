0ace33641b26d90596442b7850320079e3636e54
alonso massa <joalmr@gmail.com>
2020-02-14 01:41:51 -0500

    ok

--- lib/src/pages/mascotas_page.dart
+++ lib/src/pages/mascotas_page.dart
@@ -4,7 +4,7 @@ import 'package:proypet/src/pages/mascotas_editar_page.dart';
 import 'package:proypet/src/pages/model/mascota/mascota_model.dart';
 import 'package:proypet/src/pages/shared/appbar_menu.dart';
 import 'package:proypet/src/pages/shared/form_control/button_primary.dart';
-import 'package:proypet/src/pages/shared/styles/styles.dart';
+//import 'package:proypet/src/pages/shared/styles/styles.dart';
 
 
 // final mascotas=[
