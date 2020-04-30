39a38099546a8a4c077b235dacdc76a24000cf44
alonso massa <joalmr@gmail.com>
2020-04-29 22:42:03 -0500

    con id para play store

--- android/app/src/main/kotlin/com/example/proypet/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/proypet/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.proypet
+package com.proypet.user
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -2,4 +2,4 @@
 final String urlDevAzure = 'http://52.170.234.215/api';
 final String urlProdAzure = 'https://www.proypet.com/api';
 
-final String urlGlobal = urlDevAzure;
+final String urlGlobal = urlProdAzure;
