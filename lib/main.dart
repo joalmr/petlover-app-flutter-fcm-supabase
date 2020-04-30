6ec8319ea3a70e00b6f11564606002818be83e62
alonso massa <joalmr@gmail.com>
2020-04-30 04:37:42 -0500

    corregido

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,42 @@ import 'package:proypet/src/providers/user_provider.dart';
 import 'package:proypet/src/routes/routes.dart';
  
 
-final loginProvider = UserProvider();
-final prefs = new PreferenciasUsuario();
+// final prefs = new PreferenciasUsuario();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final prefs = new PreferenciasUsuario();
   await prefs.initPrefs();
   runApp(MyApp());
 }
 
+final loginProvider = UserProvider();
 var rutaInicio='login';
+dynamic resp;
 
-_validaStream() async {
-  var resp = await loginProvider.getUserSummary();
-  if(resp!=null) {
-    rutaInicio='navInicio';
-  }
-  else{
-    print("valor null");
-    prefs.token = '';
-    prefs.position='';
-  }
+_valida() async {
+  resp = await loginProvider.getUserSummary();
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    _validaStream();
+    final prefs = new PreferenciasUsuario();
+    
+    
+    if(prefs.token!=''){
+      _valida();
+      if(resp != null) {
+        rutaInicio='navInicio';
+      }
+
+      else{
+        print("valor null");
+        prefs.token = '';
+        prefs.position='';
+      }
+    }
 
     return MaterialApp(
       title: 'Proypet',
