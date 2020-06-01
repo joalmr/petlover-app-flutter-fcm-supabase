f8195c1fd01f257da3303bd55fbad968bd7b1a9a
alonso massa <joalmr@gmail.com>
2020-06-01 08:02:14 -0500

    fonts dentro de assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:proypet/src/bloc/provider.dart';
 import 'package:proypet/src/preferencias_usuario/preferencias_usuario.dart';
-import 'package:proypet/src/push-providers/push_provider.dart';
+// import 'package:proypet/src/push-providers/push_provider.dart';
 import 'package:proypet/src/routes/routes.dart';
 import 'package:proypet/src/shared/navigation_bar.dart';
 
