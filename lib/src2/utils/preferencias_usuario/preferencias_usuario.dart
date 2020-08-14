b2edec466a3feb4faedf21e1acf21328ee4f963c
JOSE ALONSO MASSA ROLANDO <josealonsomassarolando@MacBook-Air-de-JOSE.local>
2020-08-14 02:18:15 -0500

    cambios en preferencia de usuario

--- lib/src2/utils/preferencias_usuario/preferencias_usuario.dart
+++ lib/src2/utils/preferencias_usuario/preferencias_usuario.dart
@@ -12,52 +12,26 @@ class PreferenciasUsuario {
   SharedPreferences _prefs;
 
   void openPrefs() => initPrefs();
-
   initPrefs() async {
     this._prefs = await SharedPreferences.getInstance();
   }
 
   // GET y SET del token
-  get token {
-    return _prefs.getString('token') ?? '';
-  }
+  get token => _prefs.getString('token') ?? '';
+  set token(String value) => _prefs.setString('token', value);
 
-  set token(String value) {
-    _prefs.setString('token', value);
-  }
+  //verificado
+  get verify => _prefs.getString('verify') ?? null;
+  set verify(String value) => _prefs.setString('verify', value);
 
-//verificado
-  get verify {
-    return _prefs.getString('verify') ?? null;
-  }
+  get position => _prefs.getString('position') ?? null;
+  set position(String value) => _prefs.setString('position', value);
 
-  set verify(String value) {
-    _prefs.setString('verify', value);
-  }
+  //se usa para guardar la direccion de la ultima reserva realizada
+  get myAddress => _prefs.getString('address') ?? '';
+  set myAddress(String value) => _prefs.setString('address', value);
 
-  get position {
-    return _prefs.getString('position') ?? null;
-  }
-
-  set position(String value) {
-    _prefs.setString('position', value);
-  }
-
-//se usa para guardar la direccion de la ultima reserva realizada
-  get myAddress {
-    return _prefs.getString('address') ?? '';
-  }
-
-  set myAddress(String value) {
-    _prefs.setString('address', value);
-  }
-
-//se usa para guardar la posicion de la ultima reserva realizada
-  get myAddressLatLng {
-    return _prefs.getString('mylatlng') ?? '';
-  }
-
-  set myAddressLatLng(String value) {
-    _prefs.setString('mylatlng', value);
-  }
+  //se usa para guardar la posicion de la ultima reserva realizada
+  get myAddressLatLng => _prefs.getString('mylatlng') ?? '';
+  set myAddressLatLng(String value) => _prefs.setString('mylatlng', value);
 }
