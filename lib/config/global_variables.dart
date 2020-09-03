42bb722fb41a97b5c0c8425d3da8a53accd3395d
JOSE ALONSO MASSA ROLANDO <josealonsomassarolando@MacBook-Air-de-JOSE.local>
2020-09-03 03:42:40 -0500

    apuntando a pruebas

--- lib/config/global_variables.dart
+++ lib/config/global_variables.dart
@@ -14,7 +14,7 @@ const produccion = {
   'urlApi': 'https://proypet.com/api',
 };
 
-final bool appPruebas = false; //TODO: cambiar a false cuando sea produccion
+final bool appPruebas = true; //TODO: cambiar a false cuando sea produccion
 
 final environment = appPruebas ? pruebas : produccion;
 
