04df317b39c7006a0f253b7e25ca51b6fbd1b67c
JOSE ALONSO MASSA ROLANDO <josealonsomassarolando@MacBook-Air-de-JOSE.local>
2020-09-12 17:57:48 -0500

    info plist

--- lib/config/global_variables.dart
+++ lib/config/global_variables.dart
@@ -14,8 +14,8 @@ const produccion = {
   'urlApi': 'https://proypet.com/api',
 };
 
-//····························//TODO: cambiar google-services en android
-final bool appPruebas = true; //TODO: cambiar a false cuando sea produccion
+//····························//TODO: cambiar google-services para android
+final bool appPruebas = false; //TODO: cambiar a false cuando sea produccion
 
 final environment = appPruebas ? pruebas : produccion;
 
